f04d25a62c2fcc856d2467432076e9f0e3ebdaba
avenegas91 <aven2601@yahoo.com>
2024-01-06 20:00:12 -0500

    Accidentally did Async instead of Promise 1st.

--- part1/app.js
+++ part1/app.js
@@ -1,28 +1,22 @@
-let favoriteNumber = 11;
+let favNumber = 21;
 let baseURL = "http://numbersapi.com";
 
-// part 1
-async function part1() {
-    let data = await $.getJSON(`${baseURL}/${favoriteNumber}?json`);
-    console.log(data);
-}
-part1();
+// 1.
+$.getJSON(`${baseURL}/${favNumber}?json`).then(data => {
+  console.log(data);
+});
 
-// part 2
-const favoriteNumbers = [11, 21, 25];
-async function part2() {
-    let data = await $.getJSON(`${baseURL}/${favoriteNumbers}?json`);
-    console.log(data);
-}
-part2();
+// 2.
+let favNumbers = [7, 11, 22];
+$.getJSON(`${baseURL}/${favNumbers}?json`).then(data => {
+  console.log(data);
+});
 
-//part 3
-async function part3() {
-    let facts = await Promise.all(
-        Array.from({ length: 4 }, () => $.getJSON(`${baseURL}/${favoriteNumber}?json`))
-    );
-    facts.forEach(data => {
-        $('body').append(`<p>${data.text}</p>`);
-    });
-}
-part3();
+// 3.
+Promise.all(
+  Array.from({ length: 4 }, () => {
+    return $.getJSON(`${baseURL}/${favNumber}?json`);
+  })
+).then(facts => {
+  facts.forEach(data => $("body").append(`<p>${data.text}</p>`));
+});
